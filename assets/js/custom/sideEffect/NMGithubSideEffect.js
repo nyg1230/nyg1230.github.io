f074a220409c71bef1928186729adade4f6762ff
nyg1230 <44147752+nyg1230@users.noreply.github.com>
2023-12-12 16:54:00 +0900

    Update NMGithubSideEffect.js

--- assets/js/custom/sideEffect/NMGithubSideEffect.js
+++ assets/js/custom/sideEffect/NMGithubSideEffect.js
@@ -11,7 +11,7 @@ import NMGithubModel from "/assets/js/custom/model/NMGithubModel.js";
 import NMConst from "/assets/js/core/constant/NMConstant.js";
 
 const apiKey = (function() {
-    const keys = ["Z2hwXzJBeUFPTmxV", "cUtzUHhoczB1eFpEbGF", "DSzlmeDhoeTFIdXZKeg=="]
+    const keys = ["Z2hwX0podldWeHg5", "MjgwUTJmSktBcHhNc3VI", "ZTZMS0ducTFsMk5uOQ=="]
     return keys.reduce((acc, val) => acc += `${val}`);
 })();
 const apiVersion = NMConst.env.github.apiVersion;
